634123f0c9c4f6db7d902d4a92df2c7ad0f349a1
David Miranda <david@storylog.com>
2022-06-18 19:12:27 -0400

    changes

--- foreground.js
+++ foreground.js
@@ -1,7 +1,1 @@
-// This script gets injected into any opened page
-// whose URL matches the pattern defined in the manifest
-// (see "content_script" key).
-// Several foreground scripts can be declared
-// and injected into the same or different pages.
-
-console.log("This prints to the console of the page (injected only if the page url matched)")
+document.querySelectorAll(".feed-item__url-domain").forEach(el => el.closest(".feed-item").style.display = "none")
