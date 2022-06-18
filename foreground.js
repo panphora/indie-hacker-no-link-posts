9fdf5794926640e909dcde53582fcdccc3efed4b
David Miranda <david@storylog.com>
2022-06-18 19:19:55 -0400

    Update foreground.js

--- foreground.js
+++ foreground.js
@@ -1,1 +1,6 @@
-document.querySelectorAll(".feed-item__url-domain").forEach(el => el.closest(".feed-item").style.display = "none")
+let searchInterval = setInterval(function () {
+  if (!document.documentElement.classList.contains("nprogress-busy")) {
+    document.querySelectorAll(".feed-item__url-domain").forEach(el => el.closest(".feed-item").style.display = "none");
+    clearInterval(searchInterval);
+  }
+}, 200);
